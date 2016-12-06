7375458a56883092fe81d9dc01733dfbcfa1993b
astig13 <astynasty@yahoo.com>
2016-12-06 12:20:19 +0200

    test

--- behavior/scripts/lib/getCurrentWeather.js
+++ behavior/scripts/lib/getCurrentWeather.js
@@ -13,7 +13,7 @@ module.exports = function getCurrentWeather(weatherAPIKey, locationName, next) {
     }
 
     if (body) {
-      const parsedResult = 153
+      const parsedResult = JSON.parse(body)
       next(parsedResult)
     } else {
       next()
